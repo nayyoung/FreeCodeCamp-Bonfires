58d28064edb6266563596f03424027dfacdfa169
Naya <nayayoung92@gmail.com>
2016-11-27 12:09:50 -0500

    Create Mutations.js

---
+++ Mutations.js
@@ -0,0 +1,10 @@
+function mutation(arr) {
+  arr[0] = arr[0].toLowerCase();
+  arr[1] = arr[1].toLowerCase();
+  for(var i = 0; i < arr[1].length; i++) {
+    if(arr[0].indexOf(arr[1][i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
